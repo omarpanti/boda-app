8791c25fcc24a0965ff9168ca71a9c2a705e4e2b
jesus-panti <checa270988paom@gmail.com>
2026-09-11 14:54:28 -0600

    chore: texto confirmar asistencia

--- app/invite/[uniqueLink]/InviteClient.tsx
+++ app/invite/[uniqueLink]/InviteClient.tsx
@@ -458,7 +458,7 @@ export default function InviteClient({ guest }: { guest: GuestWithTable }) {
                 className="absolute bottom-[14%] left-1/2 transform -translate-x-1/2 group flex items-center justify-center gap-2 bg-white/40 hover:bg-white/60 backdrop-blur-sm border border-[#2c2c2c]/10 px-6 py-2.5 rounded-full transition-all duration-300 shadow-sm z-10"
               >
                 <span className="font-inter tracking-widest uppercase text-[10px] md:text-xs font-medium text-[#2C2C2C]">
-                  {isResponded ? 'Ver mi confirmación' : 'Hacer clic aquí'}
+                  {isResponded ? 'Ver mi confirmación' : 'Confirmar asistencia'}
                 </span>
               </button>
             </div>
